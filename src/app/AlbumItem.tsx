1bb76cc136006c952203f81865da1da6083d16ca
Ngọc Thắng <ngocthang.devweb@gmail.com>
2023-10-27 16:50:23 +0700

    1

--- src/app/AlbumItem.tsx
+++ src/app/AlbumItem.tsx
@@ -7,7 +7,7 @@ export default function AlbumItem({ image }: {image: string}) {
         <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
-          width={200} height={200}
+          width={1200} height={600}
           src={image}
           priority
           className={`
