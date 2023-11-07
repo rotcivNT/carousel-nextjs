e332adf7e9ffd74252443b1ba9134e08c845fe88
Ngọc Thắng <ngocthang.devweb@gmail.com>
2023-11-08 01:02:40 +0700

    u

--- src/app/AlbumItem.tsx
+++ src/app/AlbumItem.tsx
@@ -4,16 +4,32 @@ import { useState } from "react";
 export default function AlbumItem({ image }: { image: string }) {
   const [isLoading, setLoading] = useState(true);
   console.log(isLoading);
-  
+
   return (
-    <Image
-            alt=""
-            fill
-            className={`
-              !w-[600px] duration-700 ease-in-out mx-auto
-              ${isLoading ? 'blur-2xl grayscale' : 'blur-0 grayscale-0'})`}
-            src={image}
-            onLoad={() => setLoading(false)}
-          />
+    <>
+      {isLoading && (
+        <Image
+          alt=""
+          fill
+          className={`
+      !w-[600px] blur-2xl grayscale mx-auto
+      `}
+          src={`/blurImg.jpg`}
+        />
+      )}
+      <Image
+        alt=""
+        fill
+        className={`
+              !w-[600px] duration-300 ease-in-out mx-auto
+              ${
+                isLoading
+                  ? "blur-2xl grayscale bg-red-400"
+                  : "blur-0 grayscale-0 bg-[none]"
+              })`}
+        src={image}
+        onLoad={() => setLoading(false)}
+      />
+    </>
   );
 }
